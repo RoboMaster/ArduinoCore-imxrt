17ab0b76f517d83dd7b7b19d4ddff67297f71980
flame.yu <flame.yu@dji.com>
2022-09-14 17:55:40 +0800

    FIX：Modify pin labels

--- variants/ai_hub/variant.h
+++ variants/ai_hub/variant.h
@@ -18,17 +18,17 @@ extern "C"
 /////////////////////////////////////////////////////
 ///                 14PIN
 /////////////////////////////////////////////////////
-#define P0   7U           // UART3_TX
-#define P1   8U           // UART3_RX
-#define P2   9U           // SPI3_MISO
-#define P3   15U          // EXT_INT1 !!!!Deprecated
-#define P4   11U          // SPI3_CLK
+#define IO8     7U           // UART3_TX
+#define IO7     8U           // UART3_RX
+#define IO6     9U           // SPI3_MISO
+// #define P3   15U          // EXT_INT1 !!!!Deprecated
+#define IO5     11U          // SPI3_CLK
 
-#define P5   13U          // I2C1_SCL
-#define P6   14U          // I2C1_SDA
-#define P7   10U          // SPI3_MOSI
-#define P8   16U          // EXT_INT0
-#define P9   12U          // SPI3_CS0
+#define IO4     13U          // I2C1_SCL
+#define IO3     14U          // I2C1_SDA
+#define IO2     10U          // SPI3_MOSI
+#define IO_RES  16U          // EXT_INT0
+#define IO1     12U          // SPI3_CS0
 
 /////////////////////////////////////////////////////
 ///                 KEY
@@ -42,6 +42,7 @@ extern "C"
 #define USER_LED_2 2U
 #define USER_LED_3 3U
 #define USER_LED_4 4U
+
 #define LED_BUILTIN USER_LED_1
 
 /////////////////////////////////////////////////////
@@ -49,8 +50,8 @@ extern "C"
 /////////////////////////////////////////////////////
 #define USART1
 
-#define PIN_UART1_RX      P1
-#define PIN_UART1_TX      P0
+#define PIN_UART1_RX      IO7
+#define PIN_UART1_TX      IO8
 #define UART1_NUM         LPUART3
 #define UART1_IRQn        LPUART3_IRQn
 #define UART1_HANDLER     LPUART3_IRQHandler
@@ -67,8 +68,8 @@ extern "C"
 // #define UART2_IRQn        LPUART8_IRQn
 // #define UART2_HANDLER     LPUART8_IRQHandler
 
-// #define PIN_UART3_RX      P1
-// #define PIN_UART3_TX      P0
+// #define PIN_UART3_RX      IO7
+// #define PIN_UART3_TX      IO8
 // #define UART3_NUM         LPUART3
 // #define UART3_IRQn        LPUART3_IRQn
 // #define UART3_HANDLER     LPUART3_IRQHandler
@@ -77,8 +78,8 @@ extern "C"
 ///                 I2C
 /////////////////////////////////////////////////////
 #define I2C1        LPI2C1
-#define I2C1_SCL    P5
-#define I2C1_SDA    P6
+#define I2C1_SCL    IO4
+#define I2C1_SDA    IO3
 // default I2C PORT
 #define SCL         I2C1_SCL
 #define SDA         I2C1_SDA
@@ -86,10 +87,10 @@ extern "C"
 /////////////////////////////////////////////////////
 ///                 SPI
 /////////////////////////////////////////////////////
-#define SPI3_MOSI   P7
-#define SPI3_MISO   P2
-#define SPI3_SCK    P4
-#define SPI3_CS     P9
+#define SPI3_MOSI   IO2
+#define SPI3_MISO   IO6
+#define SPI3_SCK    IO5
+#define SPI3_CS     IO1
 
 #define MOSI        SPI3_MOSI
 #define MISO        SPI3_MISO
@@ -99,14 +100,14 @@ extern "C"
 ///                 OTHER
 /////////////////////////////////////////////////////
 
-#define A0  P0
-#define A1  P1
-#define A2  P2
-#define A3  P4
-#define A4  P7
-#define A5  P9
+#define A0  IO8
+#define A1  IO7
+#define A2  IO6
+#define A3  IO5
+#define A4  IO2
+#define A5  IO1
 
-#define PWM0 P8
+#define PWM0 IO_RES
 
 #ifdef __cplusplus
 }
